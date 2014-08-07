541718c9009ef3c9e556b57a916cd3f74c7b01fd
rakesh <rakesh.singh@flipkart.com>
2014-08-07 23:06:25 +0530

    first commit added

---
+++ Hackathon/src/com/flipkart/Hackathon/HActivity.java
@@ -0,0 +1,15 @@
+package com.flipkart.Hackathon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
